df3f22cea4aed06104061a37f8a7f62747e0fafb
frezc <504021398@qq.com>
2019-08-31 23:54:37 +0800

    0.1.0 && an available example

--- example/index.ts
+++ example/index.ts
@@ -1,8 +1,27 @@
 import { initCollector } from '../src';
+import prom from 'promjs';
+import { exportMetrics } from 'promjs-export';
+
+const registry = prom();
+const unsupportedCounter = registry.create('counter', 'collector_unsupported_total', 'A counter for browser support for collector');
+const setupCounter = registry.create('counter', 'collector_setup_total', 'A counter for collector setup');
+
+function sendMetrics() {
+  navigator.sendBeacon("http://localhost:8080/custom_metrics", JSON.stringify(exportMetrics(registry)));
+  registry.reset();
+}
 
 initCollector({
   callback: (entries) => {
-    console.log('collector', entries)
+    navigator.sendBeacon("http://localhost:8080/resources", JSON.stringify(entries))
+  },
+  onUnsupported: () => {
+    unsupportedCounter.inc();
+    sendMetrics();
+  },
+  onSetUp: () => {
+    setupCounter.inc();
+    sendMetrics();
   },
   throttle: 2000,
 });
